7874fc38ca0f03b9c75d1f5c0de23353c2f5d4ad
Mercy Kipyegon <mercyjemosop@gmail.com>
2020-08-03 00:02:35 +0300

    Updates

---
+++ precache-manifest.293a4c5e244c2fa1e96add5d6957e79c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df4ea3a7dbb6f63f7a0c693525a2834a",
+    "url": "/portfolio-deploy/index.html"
+  },
+  {
+    "revision": "656eb215dacecce8bdb0",
+    "url": "/portfolio-deploy/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "ecf4df40d599caf482f4",
+    "url": "/portfolio-deploy/static/css/main.dd5653d8.chunk.css"
+  },
+  {
+    "revision": "656eb215dacecce8bdb0",
+    "url": "/portfolio-deploy/static/js/2.01d3c3ab.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/portfolio-deploy/static/js/2.01d3c3ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecf4df40d599caf482f4",
+    "url": "/portfolio-deploy/static/js/main.f32cc948.chunk.js"
+  },
+  {
+    "revision": "8aaf18d69d4623215f22",
+    "url": "/portfolio-deploy/static/js/runtime-main.7097ef93.js"
+  }
+]);
